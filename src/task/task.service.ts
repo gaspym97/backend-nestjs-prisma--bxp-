db240ef3b105c8411baedf79f63e0e6681d88358
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-27 19:05:54 -0400

    setting tasks

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,15 @@
 // this module defines the conexion with the database
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Task } from '@prisma/client'
 
 @Injectable()
-export class TaskService { }
+export class TaskService {
+    // with this you can make enquiries
+    constructor(private prisma: PrismaService) { }
+
+    async getAllTasks(): Promise<Task[]> {
+        return this.prisma.task.findMany();
+    }
+
+}
